4ab6546f32302fab757892a5ec1d13833fa43df7
wuleekey <wuleekey@qq.com>
2022-03-15 00:52:36 +0800

    third commit

--- app/src/main/java/com/bytedance/jstu/demo/lesson4/homework/ClockActivity.kt
+++ app/src/main/java/com/bytedance/jstu/demo/lesson4/homework/ClockActivity.kt
@@ -2,6 +2,11 @@ package com.bytedance.jstu.demo.lesson4.homework
 
 import android.app.Activity
 import android.os.Bundle
+import android.view.MotionEvent
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.bytedance.jstu.demo.R
 
@@ -10,10 +15,29 @@ import com.bytedance.jstu.demo.R
  *  time   : 2021/10/30
  *  desc   :
  */
-class ClockActivity : AppCompatActivity() {
+class ClockActivity : AppCompatActivity(), View.OnClickListener {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_clock)
+        var auto_btn = findViewById<Button>(R.id.auto_button)
+        auto_btn.setOnClickListener(this)
+        var man_btn = findViewById<Button>(R.id.man_button)
+        man_btn.setOnClickListener(this)
+//        var tvclock = findViewById<TextView>(R.id.tv_clock)
+//        var clock = findViewById<ClockView>(R.id.clock)
+    }
+
+    override fun onClick(v: View?) {
+        when(v?.id) {
+            R.id.auto_button -> {  //自动时间
+                Toast.makeText(this, "切换自动时间", Toast.LENGTH_SHORT).show()
+//                autoTime(findViewById<ClockView>(R.id.tv_clock), findViewById<TextView>(R.id.clock))
+            }
+            R.id.man_button -> {  //手动时间
+                Toast.makeText(this, "切换手动时间", Toast.LENGTH_SHORT).show()
+
+            }
+        }
     }
 }
--- app/src/main/java/com/bytedance/jstu/demo/lesson4/homework/ClockView.kt
+++ app/src/main/java/com/bytedance/jstu/demo/lesson4/homework/ClockView.kt
@@ -8,7 +8,12 @@ import android.graphics.Path
 import android.util.AttributeSet
 import android.view.MotionEvent
 import android.view.View
-import java.lang.Math.PI
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+import com.bytedance.jstu.demo.R
+import org.w3c.dom.Text
+import java.lang.Math.*
 import java.util.*
 import kotlin.math.cos
 import kotlin.math.sin
@@ -24,9 +29,7 @@ class ClockView @JvmOverloads constructor(
     defStyleAttr: Int = 0
 ) : View(context, attrs, defStyleAttr) {
 
-    var hx: Float = 1000F
-    var hy: Float = 1000F
-
+//    private var tv: TextView = findViewById(R.id.tv_clock)
     private var hPaint: Paint = Paint()  //时针
     private var mPaint: Paint = Paint()  //分针
     private var sPaint: Paint = Paint()  //秒针
@@ -71,99 +74,102 @@ class ClockView @JvmOverloads constructor(
         wPaintBold.textAlign = Paint.Align.CENTER
     }
 
+    var curX: Float? = 0F
+    var curY: Float? = 0F
+    var hdeg: Double = 225.0  //时针角度
+    var mdeg: Double = 90.0  //分针角度
+    var sdeg: Double = 0.0  //秒针角度
+    val ox: Float = 500F  //圆心坐标
+    val oy: Float = 1000F  //圆心坐标
+    val r: Float = 500F  //半径
+
     override fun onDraw(canvas: Canvas?) {
         super.onDraw(canvas)
-        val w: Float = width.toFloat()
-        val h: Float = height.toFloat()
-        val r: Float = w / 2
-        val hr: Float = (0.4 * r).toFloat()
-        val mr: Float = (0.65 * r).toFloat()
-        val sr: Float = (0.8 * r).toFloat()
-        var hdeg: Double = 215.0
-        var mdeg: Double = 90.0
-        var sdeg: Double = -30.0
-        var x: Float = ((hr * cos(hdeg * PI / 180)) + r).toFloat()
-        var y: Float = ((hr * sin(hdeg * PI / 180)) + r).toFloat()
-        canvas?.drawLine(r, r, hx, hy, hPaint) //test时针
-        canvas?.drawLine(r, r, x, y, hPaint)
-        canvas?.drawCircle(x, y, 6F, hPaint)
-        x = ((mr * cos(mdeg * PI / 180)) + r).toFloat()
-        y = ((mr * sin(mdeg * PI / 180)) + r).toFloat()
-        canvas?.drawLine(r, r, x, y, mPaint)
-        canvas?.drawCircle(x, y, 5F, mPaint)
-        x = ((sr * cos(sdeg * PI / 180)) + r).toFloat()
-        y = ((sr * sin(sdeg * PI / 180)) + r).toFloat()
-        canvas?.drawLine(r, r, x, y, sPaint)
-        canvas?.drawCircle(x, y, 3F, sPaint)
-        canvas?.drawCircle(r, r, 8F, sPaint)
+//        Toast.makeText(context, "width=$width,height=$height", Toast.LENGTH_SHORT).show()
 
-        for(deg in 0..359 step 6) {  //画刻度线
-            val x1: Float = (0.9 * r * cos(deg.toDouble() * PI / 180)).toFloat() + r
-            val y1: Float = (0.9 * r * sin(deg.toDouble() * PI / 180)).toFloat() + r
-            val x2: Float = (r * cos(deg.toDouble() * PI / 180)).toFloat() + r
-            val y2: Float = (r * sin(deg.toDouble() * PI / 180)).toFloat() + r
+        //画时针
+        var x: Float = ((0.4 * r * cos(hdeg * PI / 180)) + ox).toFloat()
+        var y: Float = ((0.4 * r  * sin(hdeg * PI / 180)) + oy).toFloat()
+        canvas?.drawLine(ox, oy, x, y, hPaint)
+        canvas?.drawCircle(x, y, 6F, hPaint)
+
+        //画分针
+        x = ((0.65 * r * cos(mdeg * PI / 180)) + ox).toFloat()
+        y = ((0.65 * r * sin(mdeg * PI / 180)) + oy).toFloat()
+        canvas?.drawLine(ox, oy, x, y, mPaint)
+        canvas?.drawCircle(x, y, 5F, mPaint)
+
+        //画秒针
+        x = ((0.8 * r * cos(sdeg * PI / 180)) + ox).toFloat()
+        y = ((0.8 * r * sin(sdeg * PI / 180)) + oy).toFloat()
+        canvas?.drawLine(ox, oy, x, y, sPaint)
+        canvas?.drawCircle(x, y, 3F, sPaint)
+        canvas?.drawCircle(ox, oy, 10F, sPaint)
+
+        //画时间刻度线
+        for(deg in 0..354 step 6) {
+            val x1: Float = (0.9 * r * cos(deg.toDouble() * PI / 180)).toFloat() + ox
+            val y1: Float = (0.9 * r * sin(deg.toDouble() * PI / 180)).toFloat() + oy
+            val x2: Float = (r * cos(deg.toDouble() * PI / 180)).toFloat() + ox
+            val y2: Float = (r * sin(deg.toDouble() * PI / 180)).toFloat() + oy
             if(deg % 30 == 0) canvas?.drawLine(x1, y1, x2, y2, wPaintBold)
             else canvas?.drawLine(x1, y1, x2, y2, wPaint)
         }
 
-        x = ((0.8 * r * cos( 5.5 * PI / 180)) + r).toFloat()
-        y = ((0.8 * r * sin( 5.5 * PI / 180)) + r).toFloat()
+        //画时间数字
+        x = (0.8 * r * cos( 5.5 * PI / 180)).toFloat() + ox
+        y = (0.8 * r * sin( 5.5 * PI / 180)).toFloat() + oy
         canvas?.drawText("3", x, y, wPaintBold)
-        x = (0.82 * r * cos( 34 * PI / 180)).toFloat() + r
-        y = (0.82 * r * sin( 34 * PI / 180)).toFloat() + r
+        x = (0.82 * r * cos( 34 * PI / 180)).toFloat() + ox
+        y = (0.82 * r * sin( 34 * PI / 180)).toFloat() + oy
         canvas?.drawText("4", x, y, wPaintBold)
-        x = (0.84 * r * cos( 62 * PI / 180)).toFloat() + r
-        y = (0.84 * r * sin( 62 * PI / 180)).toFloat() + r
+        x = (0.84 * r * cos( 62 * PI / 180)).toFloat() + ox
+        y = (0.84 * r * sin( 62 * PI / 180)).toFloat() + oy
         canvas?.drawText("5", x, y, wPaintBold)
-        x = (0.85 * r * cos( 90 * PI / 180)).toFloat() + r
-        y = (0.85 * r * sin( 90 * PI / 180)).toFloat() + r
+        x = (0.85 * r * cos( 90 * PI / 180)).toFloat() + ox
+        y = (0.85 * r * sin( 90 * PI / 180)).toFloat() + oy
         canvas?.drawText("6", x, y, wPaintBold)
-        x = (0.84 * r * cos( 118 * PI / 180)).toFloat() + r
-        y = (0.84 * r * sin( 118 * PI / 180)).toFloat() + r
+        x = (0.84 * r * cos( 118 * PI / 180)).toFloat() + ox
+        y = (0.84 * r * sin( 118 * PI / 180)).toFloat() + oy
         canvas?.drawText("7", x, y, wPaintBold)
-        x = (0.82 * r * cos( 145 * PI / 180)).toFloat() + r
-        y = (0.82 * r * sin( 145 * PI / 180)).toFloat() + r
+        x = (0.82 * r * cos( 145 * PI / 180)).toFloat() + ox
+        y = (0.82 * r * sin( 145 * PI / 180)).toFloat() + oy
         canvas?.drawText("8", x, y, wPaintBold)
-        x = (0.8 * r * cos( 174 * PI / 180)).toFloat() + r
-        y = (0.8 * r * sin( 174 * PI / 180)).toFloat() + r
+        x = (0.8 * r * cos( 174 * PI / 180)).toFloat() + ox
+        y = (0.8 * r * sin( 174 * PI / 180)).toFloat() + oy
         canvas?.drawText("9", x, y, wPaintBold)
-        x = (0.71 * r * cos( 205 * PI / 180)).toFloat() + r
-        y = (0.71 * r * sin( 205 * PI / 180)).toFloat() + r
+        x = (0.71 * r * cos( 205 * PI / 180)).toFloat() + ox
+        y = (0.71 * r * sin( 205 * PI / 180)).toFloat() + oy
         canvas?.drawText("10", x, y, wPaintBold)
-        x = (0.69 * r * cos( 239 * PI / 180)).toFloat() + r
-        y = (0.69 * r * sin( 239 * PI / 180)).toFloat() + r
+        x = (0.69 * r * cos( 239 * PI / 180)).toFloat() + ox
+        y = (0.69 * r * sin( 239 * PI / 180)).toFloat() + oy
         canvas?.drawText("11", x, y, wPaintBold)
-        x = (0.69 * r * cos( 270 * PI / 180)).toFloat() + r
-        y = (0.69 * r * sin( 270 * PI / 180)).toFloat() + r
+        x = (0.69 * r * cos( 270 * PI / 180)).toFloat() + ox
+        y = (0.69 * r * sin( 270 * PI / 180)).toFloat() + oy
         canvas?.drawText("12", x, y, wPaintBold)
-        x = (0.72 * r * cos( 304 * PI / 180)).toFloat() + r
-        y = (0.72 * r * sin( 304 * PI / 180)).toFloat() + r
+        x = (0.72 * r * cos( 304 * PI / 180)).toFloat() + ox
+        y = (0.72 * r * sin( 304 * PI / 180)).toFloat() + oy
         canvas?.drawText("1", x, y, wPaintBold)
-        x = (0.76 * r * cos( 335 * PI / 180)).toFloat() + r
-        y = (0.76 * r * sin( 335 * PI / 180)).toFloat() + r
+        x = (0.76 * r * cos( 335 * PI / 180)).toFloat() + ox
+        y = (0.76 * r * sin( 335 * PI / 180)).toFloat() + oy
         canvas?.drawText("2", x, y, wPaintBold)
 
-//        canvas?.drawCircle(r, r, (0.9 * r).toFloat(), mPaint)
-//        canvas?.drawCircle(r, r, (0.86 * r).toFloat(), mPaint)
-//        canvas?.drawCircle(r, r, (0.7 * r).toFloat(), mPaint)
+        //画电子时钟
+        var hour = (hdeg / 30 + 3).toInt()
+        if(hour > 12) hour -= 12
+        var min: Int = (mdeg / 6 + 15).toInt() % 60
+        var sec: Int = (sdeg / 6 + 15).toInt() % 60
+        var shour = hour.toString()
+        if (hour < 10)  shour = "0" + shour
+        var smin = min.toString()
+        if (min < 10)  smin = "0" + smin
+        var ssec = sec.toString()
+        if (sec < 10)  ssec = "0" + ssec
+        canvas?.drawText("$shour : $smin : $ssec", 500F, 300F, wPaintBold)
 
-//        for(hour in 3..14) {
-//            val x1: Float = (450 * cos(i.toDouble() * PI / 180)).toFloat() + w / 2
-//            val y1: Float = (450 * sin(i.toDouble() * PI / 180)).toFloat() + h / 2
-//            val x2: Float = (500 * cos(i.toDouble() * PI / 180)).toFloat() + w / 2
-//            val y2: Float = (500 * sin(i.toDouble() * PI / 180)).toFloat() + h / 2
-//            if(t % 30 == 0) {
-//                canvas?.drawLine(x1, y1, x2, y2, wPaintBold)
-//                val path: Path = Path()
-//                path.moveTo(x1, y1)
-//                path.lineTo(x1 + 20F, y1)
-//                canvas?.drawTextOnPath("$hour", path, 0F, 0F, wPaintBold)
-//        }
-
-//        canvas?.drawLines(pts.toFloatArray(), wPaint)
 //        canvas?.drawPoint(500F,500F, mPaint)
-//        canvas?.drawLine(500F,500F, 933F, 250F, mPaint)
-//        canvas?.drawOval(200F, 200F, 100F, 100F,  mPaint)
+//        canvas?.drawLine(500F, 500F, 933F, 250F, mPaint)
+//        canvas?.drawOval(200F, 200F, 100F, 100F, mPaint)
 //        canvas?.drawText("texttext", 100F, 100F, mPaint)
 //        canvas?.drawRect(0F,0F,1000F,1000F,mPaint)
 //        canvas?.drawCircle(500F, 500F, 10F, mPaint)
@@ -171,11 +177,44 @@ class ClockView @JvmOverloads constructor(
 //        canvas?.drawArc(RectF(0F, 0F, width.toFloat(), height.toFloat()), 0F, 360F, true, wPaint)
     }
 
+    //实现手动触摸切换时间
     override fun onTouchEvent(event: MotionEvent?): Boolean {
         super.onTouchEvent(event)
-        this.hx = event?.getX()!!
-        this.hy = event.getY()
+        this.curX = event?.getX()!!
+        this.curY = event?.getY()!!
+
+        //当前触摸点相对于时钟中心的角度
+        var deg: Double = toDegrees(atan(((curY!! - oy) / (curX!! - ox)).toDouble()))
+        if(curX!! >= ox && curY!! >= oy){  //第一象限
+            //值不变
+        }else if(curX!! <= ox && curY!! >= oy){  //第二象限
+            deg += 180.0
+        }else if(curX!! <= ox && curY!! <= oy){  //第三象限
+            deg += 180.0
+        }else if(curX!! >= ox && curY!! <= oy){  //第四象限
+            deg += 360.0
+        }
+
+        //用角度来判断应该滑动哪个针
+        if(abs(deg - sdeg) <= 6 || abs(deg - sdeg) >= 354){
+            sdeg = deg
+        }else if(abs(deg - mdeg) <= 6 || abs(deg - mdeg) >= 354){
+            if(mdeg - deg >= 354)  {
+                hdeg += (deg + 360 - mdeg) / 12
+            }else if(deg - mdeg >= 354) {
+                hdeg += (deg - 360 - mdeg) / 12
+            }else {
+                hdeg += (deg - mdeg) / 12
+            }
+            mdeg = deg
+            Toast.makeText(context, "$hdeg", Toast.LENGTH_SHORT).show()
+        }else if(abs(deg - hdeg) <= 6 || abs(deg - hdeg) >= 354){
+            hdeg = deg
+        }
+
+        //重绘onDraw
         this.invalidate()
+
         return true
     }
 }
